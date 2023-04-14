d59381225cd8db87550cd0ebe3441d3400192a93
khy236 <khy236@nyu.edu>
2023-04-13 20:24:24 -0400

    first draft

---
+++ data/bridgelines.js
@@ -0,0 +1,60 @@
+const bridgelines = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    [
+                        -74.0046827818322,
+                        40.712263443290766
+                    ],
+                    [
+                        -73.98865366847019,
+                        40.69952605810158
+                    ],
+                    [
+                        -73.98858336534165,
+                        40.69638130776531
+                    ]
+                ],
+                "type": "LineString"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    [
+                        -73.99554337509105,
+                        40.71610016970064
+                    ],
+                    [
+                        -73.98513851203153,
+                        40.697660546152775
+                    ]
+                ],
+                "type": "LineString"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    [
+                        -73.98577124019042,
+                        40.71764545537684
+                    ],
+                    [
+                        -73.96039181070054,
+                        40.71007854195409
+                    ]
+                ],
+                "type": "LineString"
+            }
+        }
+    ]
+}
